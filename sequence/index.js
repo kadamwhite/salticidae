fd365e6407d9cfa1010ff6b5a170effe6f164196
K. Adam White <kadamwhite@users.noreply.github.com>
2018-06-07 23:49:50 +0100

    v0.5.0: support injecting progressBar instance

--- sequence/index.js
+++ sequence/index.js
@@ -5,12 +5,19 @@ const { chunk } = require( '../util' );
  * Given an array of functions creating Promises, execute those functions one
  * at a time and return a Promise that resolves when all functions have run.
  *
- * @param {Function[]} fns Array of functions returning promises.
+ * @param {Function[]}  fns            Array of functions returning promises.
+ * @param {ProgressBar} [progressBar]  Optional ProgressBar instance (see `progress` library).
  * @returns {Promise} Promise that resolves when the functions have all completed.
  */
-const runInSequence = ( fns, sequenceName ) => fns.reduce( async ( lastStep, fn, i ) => {
-	debug( `Running function #${ i + 1 } of ${ fns.length } in queue ${ sequenceName || '' }` );
+const runInSequence = ( fns, progressBar = null ) => fns.reduce( async ( lastStep, fn, i ) => {
+	debug( `Running function #${ i + 1 } of ${ fns.length }` );
+
 	await lastStep;
+
+	if ( progressBar && typeof progressBar.tick === 'function' ) {
+		progressBar.tick();
+	}
+
 	return fn();
 }, Promise.resolve() );
 
@@ -22,8 +29,9 @@ const runInSequence = ( fns, sequenceName ) => fns.reduce( async ( lastStep, fn,
  * @param {Function[]} fns Array of functions returning promises.
  * @param {Number} batchSize Number of functions to permit to run simultaneously.
  */
-const runInBatches = ( fns, batchSize ) => runInSequence(
-	chunk( fns, batchSize ).map( batch => () => Promise.all( batch.map( fn => fn() ) ) )
+const runInBatches = ( fns, batchSize, progressBar ) => runInSequence(
+	chunk( fns, batchSize ).map( batch => () => Promise.all( batch.map( fn => fn() ) ) ),
+	progressBar
 );
 
 module.exports = {
